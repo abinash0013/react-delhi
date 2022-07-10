b87bd7fc0ff020cc5e97ee45bcb5d5814fd511e8
abinash0013 <74831561+abinash0013@users.noreply.github.com>
2022-07-10 21:38:30 +0530

    Update resultApi.js

--- src/pages/Result/api/resultApi.js
+++ src/pages/Result/api/resultApi.js
@@ -7,72 +7,72 @@ const ResultApi = [
     {
         id: 2,
         time: 10.01,
-        number: 13,
+        number: 10,
     },
     {
         id: 3,
         time: 10.01,
-        number: 13,
+        number: 45,
     },
     {
         id: 4,
         time: 10.01,
-        number: 13,
+        number: 61,
     },
     {
         id: 5,
         time: 10.01,
-        number: 13,
+        number: 89,
     },
     {
         id: 6,
         time: 10.01,
-        number: 13,
+        number: 77,
     },
     {
         id: 7,
         time: 10.01,
-        number: 13,
+        number: 11,
     },
     {
         id: 8,
         time: 10.01,
-        number: 13,
+        number: 89,
     },
     {
         id: 9,
         time: 10.01,
-        number: 13,
+        number: 34,
     },
     {
         id: 10.01,
         time: 10.01,
-        number: 13,
+        number: 67,
     },
     {
         id: 11,
         time: 10.01,
-        number: 13,
+        number: 83,
     },
     {
         id: 12,
         time: 10.01,
-        number: 13,
+        number: 56,
     },
     {
         id: 13,
         time: 10.01,
-        number: 13,
+        number: 33,
     },
     {
         id: 14,
         time: 10.01,
-        number: 13,
+        number: 99,
     },
     {
         id: 15,
         time: 10.01,
-        number: 13,
+        number: 87,
     },
 ];
 
